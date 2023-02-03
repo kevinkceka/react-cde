a4c2319de63fa56f28cdc063fd04e9be17c42fc2
savitha krishnan <savithakrishnan2021@gmail.com>
2023-02-03 15:52:12 +0100

    commit on my branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('react');
-  const [news, setNews] = useState([]);
+  onChange(setSearchTerm(e.target.value))
+      const [news, setNews] = useState([]);
 
-  
-  useEffect(() => {
-    fetch(`https://hn.algolia.com/api/v1/search?query=${searchTerm}`)
+useEffect(() => {
+fetch(`https://hn.algolia.com/api/v1/search?query=${searchTerm}`)
       .then(response => response.json())
       .then(data => {
         setNews(data.hits);
@@ -15,24 +15,33 @@ function App() {
       .catch(err => {
         console.log(err);
       });
-  }, [searchTerm]);
-  <input>
-    type="text"
-    placeholder="Enter your Search Keyword here"
-    value={searchTerm}
-    onChange={(e) => setSearchTerm(e.target.value)}
-
-  </input>
+    }, [searchTerm]);
+    <Form>
+              <Form.Control
+                type="text"
+                placeholder="Enter your Search Keyword here"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+            </Form>
+    // <input>
+    //             type="text"
+    //             placeholder="Enter your Search Keyword here"
+    //             value={searchTerm}
+    //             onChange={(e) => setSearchTerm(e.target.value)}      
+    // </input>
   return (
-
+    
     <ul>
-      <div className="container" >
-        {
-          news.map(item1 => (<li key={item1.objectID}> {item1.title} </li>))
-        }
-      </div>
-    </ul>
+      <div className= "container" >
+       {
+       news.map( item1 => (<li key={item1.objectID}> {item1.title} </li>))
+        } 
+        </div>
 
+
+    </ul>
+    
   );
 }
 
